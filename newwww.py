29a2fdd12eaa0308168a2f5e1033fc4da1737101
REVANTHAREPALLI <revantharepalli10@gmail.com>
2025-03-15 13:38:42 +0530

    personal_file_sorter.py

--- newwww.py
+++ newwww.py
@@ -1,13 +1,16 @@
-import os,shutil
-path=r"/home/revantharepalli10/ex/"
-k=os.listdir(path)
-l=list()
-for i in k:
-   j=i.split('.')
-   if not os.path.exists(path+j[-1]):
+###  THIS IS THE CODE I PERSONALLY CREATED TO SORT THE FILES IN MY CHROMEBOOK
+
+
+import os,shutil                       #These are the modules that are used----OS(OPERATING SYSTEM)
+path=r"/home/revantharepalli/ex/"
+k=os.listdir(path)                     #GIVES LIST OF FILES IN THE GIVEN PATH
+l=list()         
+for i in k:                            #IT CREATES FOLDERS WITH THE NAMES WHICH ARE AVAILABLE AS FILE EXTENSIONS & ALSO AVOIDS DUPLICATION
+   j=i.split('.')  
+   if not os.path.exists(path+j[-1]):  # HELPS TO AVOID DUPLICATION
       os.makedirs(path+j[-1])
-      l.append(j[-1])
-for i in k:
+      l.append(j[-1])                  #LIST OF CREATED FOLDERS
+for i in k:                            #LOOP TO MOVE FILES INTO FOLDERS ACCORDING TO THEIR EXTENSIONS
    for j in l:
-      if '.'+j in i and not os.path.exists(path+j+'/'+i):
+      if '.'+j in i and not os.path.exists(path+j+'/'+i):   # HELPS TO AVOID DUPLICATION
             shutil.move(path+i,path+j+'/'+i)
